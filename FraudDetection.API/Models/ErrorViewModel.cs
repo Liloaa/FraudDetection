50c98956faa64e14d797a0fd370a066db7a4d017
Tonynyaiko <tonynyaikorabakomalala@gmail.com>
2026-07-20 07:10:00 +0200

    Membre4: Maj

--- FraudDetection.API/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FraudDetection.API.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
